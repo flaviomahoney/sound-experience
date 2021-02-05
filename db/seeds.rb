b5448aff5c3b353cec3a9446057aec2cc1970767
flaviomahoney <flavio@mahoneyt.com>
2021-02-04 21:59:11 -0300

    Created some seedsg

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Destroying tables"
+User.destroy_all
+Product.destroy_all
+
+puts "Tables Destroyed"
+puts "Creating Seeds"
+
+User.create!(email: "flavio@gmail.com", password: "123456")
+User.create!(email: "caique@gmail.com", password: "123456")
+
+Product.create!(short_description: "Caixa de Som Preta", description: "Essa seria uma caixa de som preta muito bonita")
+Product.create!(short_description: "Caixa de Som Branca", description: "Essa seria uma caixa de som branca muito bonita")
+Product.create!(short_description: "Caixa de Som Amarela", description: "Essa seria uma caixa de som amarela muito bonita")
+
+puts "Seeds Created"
