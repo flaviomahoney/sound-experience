f213c50c17a35f0a5f4fafe9839bfb67146b8e2c
flaviomahoney <flavio@mahoneyt.com>
2021-02-04 22:05:27 -0300

    Added user_id into products table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_03_162304) do
+ActiveRecord::Schema.define(version: 2021_02_05_010246) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,8 @@ ActiveRecord::Schema.define(version: 2021_02_03_162304) do
     t.string "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_products_on_user_id"
   end
 
   create_table "requests", force: :cascade do |t|
