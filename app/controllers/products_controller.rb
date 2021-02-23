7cb348e513534f2b6cb04298000d019aa30bcc42
flaviomahoney <flavio@mahoneyt.com>
2021-02-23 20:14:50 -0300

    Created the method create and new to products controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,4 +6,26 @@ class ProductsController < ApplicationController
     def show
         @product = Product.find(params[:id])
     end
+
+    def new
+        @product = Product.new
+    end
+
+    def create
+        @product = Product.new(product_params)
+        if @product.save
+          flash[:success] = "product successfully created"
+          redirect_to @product
+        else
+          flash[:error] = "Something went wrong"
+          render 'new'
+        end
+    end
+
+    private
+
+    def product_params
+        params.require(:product).permit(:short_description, :description)
+    end
+    
 end
