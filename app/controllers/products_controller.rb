32c13ac9a3feadab3ed20e17212e886b3a090a15
flaviomahoney <flavio@mahoneyt.com>
2021-02-24 18:43:09 -0300

    Added destroy to products controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -36,6 +36,18 @@ class ProductsController < ApplicationController
           render 'edit'
         end
     end
+
+    def destroy
+      @product = Product.find(params[:id])
+      if @product.destroy
+        flash[:success] = 'Object was successfully deleted.'
+        redirect_to products_url
+      else
+        flash[:error] = 'Something went wrong'
+        redirect_to products_url
+      end
+    end
+    
     
     
 
