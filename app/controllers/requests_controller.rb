8b8c10c24608b1daef5739d894dbd90a3acf600a
flaviomahoney <flavio@mahoneyt.com>
2021-03-04 19:37:45 -0300

    Did the create in request controller and created a form to create a request in show

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,3 +1,22 @@
 class RequestsController < ApplicationController
-    
+    def index
+        @requests = Request.all
+    end
+
+    def create
+        @request = Request.new(request_params)
+        @product = Product.find(params[:product_id])
+        @request.product = @product
+        if @request.save
+            redirect_to products_path
+        else
+            flash[:alert] = "Something went wrong"
+        end
+    end
+
+    private
+
+    def request_params
+        params.require(:request).permit(:quantity)
+    end
 end
